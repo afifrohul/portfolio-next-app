66c47d3d3d3b751579c73975d574bdecb33c9cd0
afifrohul <afifmemyself22@gmail.com>
2025-09-03 22:10:08 +0700

    fix src image from be

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,10 +1,15 @@
 "use client";
 
+import axios from "axios";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export default function Header() {
+  const [profile, setProfile] = useState(null);
+  const [loading, setLoading] = useState(true);
   const pathname = usePathname();
 
   const navItems = [
@@ -13,6 +18,33 @@ export default function Header() {
     { href: "/experiences", label: "experiences" },
     { href: "/projects", label: "projects" },
   ];
+
+  useEffect(() => {
+    async function fetchProfile() {
+      try {
+        setLoading(true);
+        const res = await axios.get("/internal/profiles");
+        const firstSrc = res.data?.[0]?.src || null;
+        setProfile(firstSrc);
+      } catch (err) {
+        if (err.response) {
+          console.error(
+            "Server error:",
+            err.response.status,
+            err.response.data
+          );
+        } else if (err.request) {
+          console.error("No response received:", err.request);
+        } else {
+          console.error("Error setting up request:", err.message);
+        }
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchProfile();
+  }, []);
 
   return (
     <>
@@ -26,7 +58,13 @@ export default function Header() {
           {navItems.map((item) =>
             item.icon ? (
               <Link key={item.href} href={item.href}>
-                <Image src={item.icon} width={14} height={14} alt="logo" className="w-3 h-3 md:w-6 md:h-6" />
+                <Image
+                  src={item.icon}
+                  width={14}
+                  height={14}
+                  alt="logo"
+                  className="w-3 h-3 md:w-6 md:h-6"
+                />
               </Link>
             ) : (
               <Link
@@ -45,13 +83,19 @@ export default function Header() {
         </div>
       </div>
       <div>
-        <Image
-          src="https://etgcuhmmwxjmesnryxir.supabase.co/storage/v1/object/public/portfolio/profile/profile.png"
-          alt="Profile Picture"
-          width={72}
-          height={72}
-          className="w-12 h-12 sm:w-24 sm:h-24 md:w-32 md:h-32 object-cover rounded-full"
-        />
+        {loading ? (
+          <Skeleton className="w-12 h-12 sm:w-24 sm:h-24 md:w-32 md:h-32 rounded-full" />
+        ) : (
+          profile && (
+            <Image
+              src={profile}
+              alt="Profile Picture"
+              width={72}
+              height={72}
+              className="w-12 h-12 sm:w-24 sm:h-24 md:w-32 md:h-32 object-cover rounded-full"
+            />
+          )
+        )}
       </div>
     </>
   );
