cb727dbc18f102990f2ee17ead46f557f434d55f
afifrohul <afifmemyself22@gmail.com>
2025-10-09 20:53:10 +0700

    adjust icon in dashboard card

--- src/components/dashboard-card.jsx
+++ src/components/dashboard-card.jsx
@@ -1,9 +1,9 @@
 export default function DashboardCard({ icon, title, count, desc }) {
   return (
     <div className="space-y-2 border p-4 rounded-lg">
-      <div className="flex gap-2 items-center">
-        {icon}
+      <div className="flex gap-2 justify-between items-center">
         <p className="italic text-sm">{title}</p>
+        {icon}
       </div>
       <div>
         <h1 className="text-4xl font-semibold">{count}</h1>
