4cad81f4722e634da6508dfcc40c78f360d10280
afifrohul <afifmemyself22@gmail.com>
2026-01-15 09:53:16 +0700

    update timeline component experiences

--- src/components/skeleton-loader/experiences.jsx
+++ src/components/skeleton-loader/experiences.jsx
@@ -1,38 +1,75 @@
 import { Skeleton } from "@/components/ui/skeleton";
-import { Separator } from "@/components/ui/separator";
 
 export default function SkeletonExperiences() {
   return (
-    <div className="flex flex-col gap-4 mt-20 mx-auto">
-      <div className="w-fit space-y-1 mb-4">
-        <Skeleton className="w-[130px] h-6 rounded-sm" />
-        <Separator></Separator>
+    <div className="flex flex-col gap-6 mt-20 mx-auto">
+      <div className="md:grid grid-cols-3">
+        <div>
+          <Skeleton className="w-2/3 h-3 mb-2" />
+        </div>
+        <div className="space-y-2 col-span-2">
+          <Skeleton className="w-full h-3" />
+          <Skeleton className="w-full h-2" />
+          <Skeleton className="w-full h-2" />
+          <Skeleton className="w-full h-2" />
+          <Skeleton className="w-[220px] md:w-[400px] h-2" />
+        </div>
       </div>
-      <div className="space-y-6 border-l-2 pl-4">
-        <div className="space-y-2">
+      <div className="md:grid grid-cols-3">
+        <div>
+          <Skeleton className="w-2/3 h-3 mb-2" />
+        </div>
+        <div className="space-y-2 col-span-2">
           <Skeleton className="w-full h-3" />
-          <Skeleton className="w-[200px] md:w-[340px] h-3" />
+          <Skeleton className="w-full h-2" />
           <Skeleton className="w-full h-2" />
           <Skeleton className="w-full h-2" />
           <Skeleton className="w-[220px] md:w-[400px] h-2" />
         </div>
-        <div className="space-y-2">
+      </div>
+      <div className="md:grid grid-cols-3">
+        <div>
+          <Skeleton className="w-2/3 h-3 mb-2" />
+        </div>
+        <div className="space-y-2 col-span-2">
           <Skeleton className="w-full h-3" />
-          <Skeleton className="w-[200px] md:w-[340px] h-3" />
+          <Skeleton className="w-full h-2" />
           <Skeleton className="w-full h-2" />
           <Skeleton className="w-full h-2" />
           <Skeleton className="w-[220px] md:w-[400px] h-2" />
         </div>
-        <div className="space-y-2">
+      </div>
+      <div className="md:grid grid-cols-3">
+        <div>
+          <Skeleton className="w-2/3 h-3 mb-2" />
+        </div>
+        <div className="space-y-2 col-span-2">
           <Skeleton className="w-full h-3" />
-          <Skeleton className="w-[200px] md:w-[340px] h-3" />
+          <Skeleton className="w-full h-2" />
           <Skeleton className="w-full h-2" />
           <Skeleton className="w-full h-2" />
           <Skeleton className="w-[220px] md:w-[400px] h-2" />
         </div>
-        <div className="space-y-2">
+      </div>
+      <div className="md:grid grid-cols-3">
+        <div>
+          <Skeleton className="w-2/3 h-3 mb-2" />
+        </div>
+        <div className="space-y-2 col-span-2">
           <Skeleton className="w-full h-3" />
-          <Skeleton className="w-[200px] md:w-[340px] h-3" />
+          <Skeleton className="w-full h-2" />
+          <Skeleton className="w-full h-2" />
+          <Skeleton className="w-full h-2" />
+          <Skeleton className="w-[220px] md:w-[400px] h-2" />
+        </div>
+      </div>
+      <div className="md:grid grid-cols-3">
+        <div>
+          <Skeleton className="w-2/3 h-3 mb-2" />
+        </div>
+        <div className="space-y-2 col-span-2">
+          <Skeleton className="w-full h-3" />
+          <Skeleton className="w-full h-2" />
           <Skeleton className="w-full h-2" />
           <Skeleton className="w-full h-2" />
           <Skeleton className="w-[220px] md:w-[400px] h-2" />
